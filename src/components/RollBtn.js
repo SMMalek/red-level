c275984a5888355fe296569f9547a596e7420674
SMMAlek <sherif.m.malek@gmail.com>
2022-03-07 21:38:15 +0100

    fix-resp

--- src/components/RollBtn.js
+++ src/components/RollBtn.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
   const RollBtn = props => {
-    const handleClick = e => props.roll(e.target.value)
 
     let disabled = false
     let btnStyle = "w-30 font-semibold text-black hover:text-redmenta transition-all ease-in-out duration-300"
+    const handleClick = e => {
+      props.roll(e.target.value)
+      btnStyle = "w-30 font-semibold text-black hover:text-redmenta transition-all ease-in-out duration-300"
+    }
 
     if(props.text === "Next" && props.current === 2){
       disabled=true
