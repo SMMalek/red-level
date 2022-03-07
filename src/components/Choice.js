76e0e0834ae65b467b104571899775fee64afd71
SMMAlek <sherif.m.malek@gmail.com>
2022-03-07 18:30:38 +0100

    add-b

--- src/components/Choice.js
+++ src/components/Choice.js
@@ -6,14 +6,14 @@ import React from 'react'
       {
         props.setAnswersList(props.text,props.current)
     }
-    let x="flex m-auto border-2 transition-colors ease-in duration-200 border-dashed rounded-lg border-slate-300 hover:bg-redmenta/10 w-fit justify-center p-5"
+    let btnStyle="flex m-auto border-2 transition-colors ease-in duration-200 border-dashed rounded-lg border-slate-300 hover:bg-redmenta/10 w-fit justify-center p-5"
     if(props.selected === props.text){
-      x = "flex m-auto border-2 transition-colors ease-in duration-200 border-dashed rounded-lg border-slate-900 bg-redmenta/10 w-fit justify-center p-5"
+      btnStyle = "flex m-auto border-2 transition-colors ease-in duration-200 border-dashed rounded-lg border-slate-900 bg-redmenta/10 w-fit justify-center p-5"
     }
 
     return (
-      <button className={x} onClick={handleClick}>
-        <button className="m-auto text-3xl w-full h-12 w-fit justify-center">{props.text}</button>
+      <button className={btnStyle} onClick={handleClick}>
+        <p className="m-auto pt-1 text-3xl w-full h-12 w-fit justify-center">{props.text}</p>
       </button>
     );
   }
